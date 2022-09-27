e92bfeb51755f21c33f41a3f7674c520f24c5d2a
Hien Nguyen <hnguyen110.akc@gmail.com>
2022-09-26 21:50:52 -0400

    Finished creating the quiz participant database model

--- quiz_app/models/__init__.py
+++ quiz_app/models/__init__.py
@@ -1,3 +1,4 @@
 from . import question
 from . import question_solution
 from . import quiz
+from . import quiz_participant
---
+++ quiz_app/models/quiz_participant.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.db import models
+
+from quiz_app.models.quiz import Quiz
+
+
+class QuizParticipant(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='assigned_quizzes')
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='participants')
+    isComplete = models.BooleanField(default=False)
