0f3bf1cd9cb1af4dd25fe0afa9152214734af965
Hien Nguyen <hnguyen110.akc@gmail.com>
2022-10-05 12:44:32 -0400

    Fix the API to retrieve the list of assigned quizzes to include the participant id

--- quiz_app/models/user_answer.py
+++ quiz_app/models/user_answer.py
@@ -6,7 +6,10 @@ from quiz_app.models.quiz_participant import QuizParticipant
 
 
 class UserAnswer(models.Model):
+    class Meta:
+        unique_together = ('question', 'selected_solution', 'participant')
+
     result = models.FloatField(default=0)
-    question = models.OneToOneField(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     selected_solution = models.ForeignKey(QuestionSolution, on_delete=models.CASCADE)
     participant = models.ForeignKey(QuizParticipant, on_delete=models.CASCADE, related_name='answers')
