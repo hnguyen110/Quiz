abb6335604fe7dd95b76e5a8a90180a914ed39f3
Hien Nguyen <hnguyen110.akc@gmail.com>
2022-10-05 19:18:57 -0400

    Fixed the APIs to update and create answers to mark the submission correctly

--- quiz_app/serializers/user_answer/update_user_answers_serializer.py
+++ quiz_app/serializers/user_answer/update_user_answers_serializer.py
@@ -21,6 +21,21 @@ class UpdateUserAnswersSerializer(serializers.Serializer):
             ]
             UserAnswer.objects.bulk_update(answers, ['selected_solution'])
             QuizParticipant.objects.filter(pk=participant_id).update(isComplete=True)
+            saved_answers = UserAnswer \
+                .objects \
+                .filter(participant_id=participant_id,
+                        question__in=[item['question']
+                                      for item in self.validated_data['answers']]) \
+                .select_related('selected_solution')
+            for answer in saved_answers:
+                if answer.selected_solution.isCorrect:
+                    answer.result = 1
+                    answer.save()
+                else:
+                    UserAnswer \
+                        .objects \
+                        .filter(question=answer.question, participant=answer.participant) \
+                        .update(result=0)
             return answers
 
     def update(self, instance, validated_data):
